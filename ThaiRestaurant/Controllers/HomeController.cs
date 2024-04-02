4a1125087708d293a68244e934483429aa895870
aschnarre1 <aschnarre@gmail.com>
2024-04-02 11:29:04 -0700

    test branch

--- ThaiRestaurant/Controllers/HomeController.cs
+++ ThaiRestaurant/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace ThaiRestaurant.Controllers
             _logger = logger;
             _context = context;
         }
-
+        //test
         public IActionResult Index()
         {
             var dishes = _context.GetDishes(); 
